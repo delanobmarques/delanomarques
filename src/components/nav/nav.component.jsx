32b4ec2c2d020ddbe55d8430f89f265c7779ec31
delanobmarques <delanobmarques@gmail.com>
2022-07-12 12:07:08 -0300

    add mui-material to use tooltip in the nav component

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './nav.styles.scss';
 import { FcHome, FcBusinessman, FcServices, FcMultipleDevices, FcContacts } from 'react-icons/fc';
-import { useState } from 'react';
+import Tooltip from '@mui/material/Tooltip';
+
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState('#');
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FcHome/></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FcBusinessman/></a>
-      <a href="#skills" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FcServices/></a>      
-      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FcMultipleDevices/></a>      
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><FcContacts/></a>
+      <Tooltip title="Home" placement="top" arrow>
+        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FcHome/></a>
+      </Tooltip>
+      <Tooltip title="About" placement="top" arrow>
+        <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FcBusinessman/></a>
+      </Tooltip>
+      <Tooltip title="Skills" placement="top" arrow>
+        <a href="#skills" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FcServices/></a>      
+      </Tooltip>
+      <Tooltip title="Portfolio" placement="top" arrow>
+        <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><FcMultipleDevices/></a>      
+      </Tooltip>
+      <Tooltip title="Contact" placement="top" arrow>
+        <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><FcContacts/></a>
+      </Tooltip>
     </nav>
   )
 }
