dae5099a2efd7081fa571529c8bf0024464d940e
delanobmarques <delanobmarques@gmail.com>
2022-07-12 10:20:35 -0300

    code and style footer component

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './footer.styles.scss';
+import {FaFacebookF} from 'react-icons/fa';
+import {FiInstagram} from 'react-icons/fi';
 
 const Footer = () => {
   return (
-    <section id='footer'>Footer</section>
+    <footer>
+      <a href="#" className="footer__logo">Delano Marques</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.instagram.com/delanobmarques/"><FiInstagram/></a>
+        <a href="https://www.facebook.com/delano.brandesmarques/"><FaFacebookF/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Delano Marques 2022</small>
+      </div>
+    </footer>
   )
 }
 
